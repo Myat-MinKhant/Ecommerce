56f9d94ba02875cc7fb08aa8425af2c9e716a09b
Myat-MinKhant <myatminkhant.hee@gmail.com>
2023-07-26 23:10:04 +0630

    Make Cart Page Responsive

--- frontend/src/pages/Cart.jsx
+++ frontend/src/pages/Cart.jsx
@@ -35,45 +35,107 @@ const Cart = () => {
     }
 
     return (
-        <>
+        <div>
             {cartItems.length === 0 ? (
                 <div className=' bg-second w-screen h-cart'>
                     <Container className='flex'>
-                        <Box className='w-1/2 h-48 py-10 mt-10 rounded-xl bg-white px-10 ml-6 shadow-xl'>
+                        <Box className='w-full laptop:w-1/2 h-48 pt-10 mt-5 pl-5 tablet:py-10 tablet:mt-10 rounded-xl bg-white tablet:px-10 tablet:ml-6 shadow-xl'>
                             <Typography className=' text-3xl font-bold pl-4'>Cart</Typography>
                             <Typography className=' text-xl pl-4 mt-5'>Your cart is empty</Typography>
                         </Box>
                     </Container>
                 </div>
             ) : (
-                    <div className=' bg-second w-screen h-fit'>
-                        <Container className='flex h-fit'>
-                            <Box className='w-4/6 py-10 my-10 rounded-xl bg-white px-10 ml-6 shadow-xl'>
-                                <TableContainer component={Paper} >
-                                    <Table aria-label="spanning-table">
-                                        <TableHead>
-                                            <Typography className=' text-3xl font-extrabold pl-4'>Cart</Typography>
-                                            <TableRow>
-                                                <TableCell className='uppercase'>Product</TableCell>
-                                                <TableCell className='uppercase' align="center">Quantity</TableCell>
-                                                <TableCell className='uppercase' align="right">Unit Price</TableCell>
-                                                <TableCell className='uppercase' align="right">Price</TableCell>
+                <div className=' bg-second w-full h-fit'>
+                    <Container className='laptop:flex h-fit py-10 laptop:px-14 '>
+                        <Box className=' laptop:w-4/6 pt-10 mb-10 rounded-xl bg-white laptop:px-10 laptop:ml-6 shadow-xl'>
+                            <TableContainer component={Paper} className=' mobile:hidden tablet:block '>
+                                <Table aria-label="spanning-table">
+                                    <TableHead>
+                                        <Typography className='font-main text-3xl font-extrabold laptop:my-7    pl-4'>Cart</Typography>
+                                        <TableRow>
+                                            <TableCell className='uppercase col-span-1'>Product</TableCell>
+                                            <TableCell className='uppercase col-span-1' align="center">Quantity</TableCell>
+                                            <TableCell className='uppercase' align="right">Unit Price</TableCell>
+                                            <TableCell className='uppercase col-span-1' align="right">Price</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {cartItems.map(item => (
+                                            <TableRow className='cart_product_list' key={item.id}>
+                                                <TableCell>
+                                                    <Box className=' flex w-full h-40 border-solid border-2 border-second rounded-xl'>
+                                                        <img
+                                                            className=' w-30 h-24 m-auto'
+                                                            alt=''
+                                                            src={item.image}
+                                                        />
+                                                    </Box>
+                                                    <Typography className='text-xl font-main'>{item.name}</Typography>
+                                                </TableCell>
+                                                <TableCell align="right" className=' text-lg'>
+                                                    <Button
+                                                        variant="contained"
+                                                        className='min-w-fit min-h-fit bg-gray-300 text-dark shadow-none font-bold mr-2'
+                                                        onClick={() => handleRemoveFromCart(item)}>
+                                                        -
+                                                    </Button>
+                                                    {item.quantity}
+                                                    <Button
+                                                        variant="contained"
+                                                        className='min-w-fit min-h-fit bg-gray-300 text-dark shadow-none font-bold ml-2'
+                                                        onClick={() => dispatch(increaseItemQuantity(item))}>
+                                                        +
+                                                    </Button>
+                                                </TableCell>
+                                                <TableCell align="right" className=' text-lg font-main'>${item.price}</TableCell>
+                                                <TableCell align="right"
+                                                    className=' text-lg font-main'>${item.price * item.quantity}
+                                                </TableCell>
                                             </TableRow>
-                                        </TableHead>
-                                        <TableBody>
-                                            {cartItems.map(item => (
-                                                <TableRow className='cart_product_list' key={item.id}>
-                                                    <TableCell>
-                                                        <Box className=' flex w-full h-40 border-solid border-2 border-second rounded-xl'>
-                                                            <img
-                                                                className=' w-30 h-24 m-auto'
-                                                                alt=''
-                                                                src={item.image}
-                                                            />
-                                                        </Box>
-                                                        <Typography className='text-xl '>{item.name}</Typography>
-                                                    </TableCell>
-                                                    <TableCell align="right" className=' text-lg'>
+                                        ))}
+                                        <TableRow>
+                                            <TableCell rowSpan={3} />
+                                            <TableCell align='right' colSpan={2} className=' text-xl'>Subtotal</TableCell>
+                                            <TableCell align="right" className=' text-2xl'>
+                                                ${cartSubTotal}
+                                            </TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell align='right' colSpan={2} className=' text-xl col-span-2'>Shipping</TableCell>
+                                            <TableCell align="right" className=' text-2xl'>${shippingAmount}</TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell align='right' colSpan={2} className=' text-xl font-extrabold col-span-2'>Total</TableCell>
+                                            <TableCell align="right" className=' text-2xl font-extrabold '>${calculateFinalTotal()}</TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                            <TableContainer component={Paper} className=" tablet:hidden w-full">
+                                <Table aria-label="spanning-table">
+                                    <TableHead>
+                                        <Typography className='font-main text-3xl font-extrabold pl-4'>Cart</Typography>
+                                        <TableRow>
+                                            <TableCell className='uppercase col-span-1'>Product</TableCell>
+                                            <TableCell className='uppercase col-span-1' align="right">Quantity&Price</TableCell>
+                                        </TableRow>
+                                    </TableHead>
+                                    <TableBody>
+                                        {cartItems.map(item => (
+                                            <TableRow className='cart_product_list' key={item.id}>
+                                                <TableCell>
+                                                    <Box className=' flex w-full h-34 border-solid border-2 border-second rounded-xl'>
+                                                        <img
+                                                            className=' w-30 h-24 m-auto'
+                                                            alt=''
+                                                            src={item.image}
+                                                        />
+                                                    </Box>
+                                                    <Typography className='text-lg font-main'>{item.name}</Typography>
+                                                </TableCell>
+                                                <TableCell align="right" className=' text-lg flex flex-col items-end h-52 justify-around'>
+                                                    <Box>
                                                         <Button
                                                             variant="contained"
                                                             className='min-w-fit min-h-fit bg-gray-300 text-dark shadow-none font-bold mr-2'
@@ -87,58 +149,59 @@ const Cart = () => {
                                                             onClick={() => dispatch(increaseItemQuantity(item))}>
                                                             +
                                                         </Button>
-                                                    </TableCell>
-                                                    <TableCell align="right" className=' text-lg'>${item.price}</TableCell>
-                                                    <TableCell align="right"
-                                                        className=' text-lg'>${item.price * item.quantity}</TableCell>
-                                                </TableRow>
-                                            ))}
-                                            <TableRow>
-                                                <TableCell rowSpan={3} />
-                                                <TableCell colSpan={2} className=' text-xl'>Subtotal</TableCell>
-                                                <TableCell align="right" className=' text-2xl'>
-                                                    ${cartSubTotal}
+                                                    </Box>
+
+                                                    <Typography className=' text-2xl font-main'>
+                                                        ${item.price * item.quantity}
+                                                    </Typography>
                                                 </TableCell>
                                             </TableRow>
-                                            <TableRow>
-                                                <TableCell className=' text-xl' colSpan={2}>Shipping</TableCell>
-                                                <TableCell align="right" className=' text-2xl'>${shippingAmount}</TableCell>
-                                            </TableRow>
-                                            <TableRow>
-                                                <TableCell colSpan={2} className=' text-xl font-extrabold '>Total</TableCell>
-                                                <TableCell align="right" className=' text-2xl font-extrabold '>${calculateFinalTotal()}</TableCell>
-                                            </TableRow>
-                                        </TableBody>
-                                    </Table>
-                                </TableContainer>
-                            </Box>
+                                        ))}
+                                        <TableRow>
+                                            <TableCell align='right' className=' text-xl'>Subtotal</TableCell>
+                                            <TableCell align="right" className=' text-2xl'>
+                                                ${cartSubTotal}
+                                            </TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell align='right' className=' text-xl'>Shipping</TableCell>
+                                            <TableCell align="right" className=' text-2xl'>${shippingAmount}</TableCell>
+                                        </TableRow>
+                                        <TableRow>
+                                            <TableCell align='right' className=' text-xl font-extrabold'>Total</TableCell>
+                                            <TableCell align="right" className=' text-2xl font-extrabold '>${calculateFinalTotal()}</TableCell>
+                                        </TableRow>
+                                    </TableBody>
+                                </Table>
+                            </TableContainer>
+                        </Box>
 
 
-                            <Box className='w-3/6'>
-                                <Box className='flex flex-col gap-2 bg-white pt-16 py-10 rounded-xl w-96 ml-10 mt-10 shadow-xl' position={'sticky'}>
-                                    <Typography className=' text-3xl w-5/6 mx-auto font-extrabold mb-6'>Order information</Typography>
-                                    <TextField sx={style}
-                                        variant="outlined" size='small' placeholder='Name' className='w-5/6 mx-auto' />
-                                    <TextField sx={style}
-                                        variant="outlined" size='small' placeholder='Gmail@gmail.com' className='w-5/6 mx-auto' />
-                                    <Box className='flex w-5/6 mx-auto gap-2'>
-                                        <TextField sx={style} size='small' placeholder='Street' className='w1/2' />
-                                        <TextField sx={style} size='small' placeholder='City Code' className='w1/2 ' />
-                                    </Box>
-                                    <TextField sx={style}
-                                        variant="outlined" size='small' placeholder='City' className='w-5/6 mx-auto' />
-                                    <TextField sx={style}
-                                        variant="outlined" size='small' placeholder='Country' className='w-5/6 mx-auto text-dark' />
-                                    <Button className='w-5/6 mx-auto bg-black rounded-md' size='large'>
-                                        <Typography component={'p'} className=' text-md capitalize tracking-widest text-white'>Continue to payment</Typography>
-                                    </Button>
+                        <Box className='laptop:w-3/6'>
+                            <Box className='flex flex-col gap-2 bg-white pt-16 rounded-xl laptop:w-96 pb-10 laptop:ml-10 mt-10 laptop:mt-0 shadow-xl' position={'sticky'}>
+                                <Typography className=' text-3xl w-5/6 mx-auto font-extrabold mb-6'>Order information</Typography>
+                                <TextField sx={style}
+                                    variant="outlined" size='small' placeholder='Name' className='w-5/6 mx-auto' />
+                                <TextField sx={style}
+                                    variant="outlined" size='small' placeholder='Gmail@gmail.com' className='w-5/6 mx-auto' />
+                                <Box className='flex w-5/6 mx-auto gap-2'>
+                                    <TextField sx={style} size='small' placeholder='Street' className=' laptop:w1/2 w-full' />
+                                    <TextField sx={style} size='small' placeholder='City Code' className=' laptop:w1/2 w-full' />
                                 </Box>
+                                <TextField sx={style}
+                                    variant="outlined" size='small' placeholder='City' className='w-5/6 mx-auto' />
+                                <TextField sx={style}
+                                    variant="outlined" size='small' placeholder='Country' className='w-5/6 mx-auto text-dark' />
+                                <Button className='w-5/6 mx-auto bg-black rounded-md' size='large'>
+                                    <Typography component={'p'} className=' text-md capitalize tracking-widest text-white'>Continue to payment</Typography>
+                                </Button>
                             </Box>
-                        </Container>
-                    </div>
-                )
+                        </Box>
+                    </Container>
+                </div>
+            )
             }
-        </>
+        </div>
     )
 }
 
